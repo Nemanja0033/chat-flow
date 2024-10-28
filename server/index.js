9f55e5cc2861531c308cb953514b19ee409d2c5a
nemanja <antonijevicnemanja68@gmail.com>
2024-10-28 12:16:56 +0100

    server config

--- server/index.js
+++ server/index.js
@@ -1,15 +1,20 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
 const http = require('http');
-const cors = require('cors')
+const cors = require('cors');
 const { Server } = require("socket.io");
+
+const PORT = process.env.PORT || 3001;
+const ORIGIN = process.env.ORIGIN || "http://localhost:5173";
+
 app.use(cors());
 
 const server = http.createServer(app);
-
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: ORIGIN,
         methods: ["GET", "POST"],
     }
 });
@@ -20,19 +25,17 @@ io.on("connection", (Socket) => {
     Socket.on("join_room", (data) => {
         Socket.join(data);
         console.log(`User with ID: ${Socket.id} joined a room ID: ${data}`);
-    })
+    });
 
     Socket.on("send_message", (data) => {
         Socket.to(data.room).emit("receive_message", data);
-    })
+    });
 
     Socket.on("disconnect", () => {
-        console.log(`User Disconnected: ${Socket.id}`)
+        console.log(`User Disconnected: ${Socket.id}`);
     });
 });
 
-
-
-server.listen(3001, () => {
-    console.log('Server running!');
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}!`);
 });
